75bc315da94097049053f557ef0c23a1388ac365
pinchi639 <lei16009@byui.edu>
2019-10-19 20:51:57 -0600

    finished

--- lesson5/javascript/main.js
+++ lesson5/javascript/main.js
@@ -42,3 +42,25 @@ console.log(currentDateString);
 
 document.getElementById('currentDate').innerHTML = currentDateString;
 
+/*five day forecast
+
+let currentDate = weekDayNumber;
+for (let i = 1; i < 6; i++) {  
+
+// add one day
+currentDate++;
+
+// if current day os greater than 6 (Saturday), reser it to 0(Sunday)
+if (currentDate > 6) {
+    currentDate = 0;
+}
+// asign value to to placeholder in HTML
+const element = document.getElementById(`day${i}`);
+
+element.innerHTML = daysOfWeek[currentDate];
+}
+*/
+//show/hide panckes
+if (weekDayNumber === 5) {
+    document.getElementById("pancake").removeAttribute("class", "hidden");
+}
