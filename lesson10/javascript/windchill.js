cbba861af123c63410acab25c41366bdcd669d17
pinchi639 <lei16009@byui.edu>
2019-11-26 12:07:50 -0700

    preston

--- lesson10/javascript/windchill.js
+++ lesson10/javascript/windchill.js
@@ -1,18 +1,17 @@
-let currentTemp = 41;
-let windSpeed = 8;
-let humidity = 41;
-let windChill = currentTemp;
-document.getElementById("currentTemp").innerHTML = currentTemp + '&deg;';
-document.getElementById("windSpeed").innerHTML = windSpeed + 'mph';
-document.getElementById("humidity").innerHTML = humidity + '%';
+function windChill() {
+    let currentTemp = parseInt(document.getElementById("temperature").textContent);
+    let windSpeed = parseInt(document.getElementById("windSpeed").textContent);
+    let windChill = currentTemp;
 
+    if (currentTemp < 50 && windSpeed > 3) {
+        console.log("Data" + currentTemp, windSpeed);
+        windChill = 35.74 + (0.6215 * currentTemp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * currentTemp * Math.pow(windSpeed, 0.16));
+        document.getElementById("windChill").innerHTML = windChill.toFixed(1) + '&deg;';
+    }
 
-if (currentTemp < 50 && windSpeed > 3) {
-    console.log(currentTemp, windSpeed);
-    windChill = 35.74 + (0.6215 * currentTemp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * currentTemp * Math.pow(windSpeed, 0.16));
-    document.getElementById("windChill").innerHTML = windChill.toFixed(1) + '&deg;';
-}
-
-if (windChill === 0) {
-    document.getElementById("windChill").innerHTML = windChill + '&dep;';
+    if (windChill === 0) {
+        document.getElementById("windChill").innerHTML = windChill + '&deg;';
+    }
+    document.getElementById("windChill").innerHTML = windChill + '&deg;';
+    console.log("here");
 }
