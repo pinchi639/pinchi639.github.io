fbd426b4e5ccc06b6395fc2c9bed1fa690bfbcc0
pinchi639 <lei16009@byui.edu>
2021-03-15 22:58:52 -0400

    other changes

--- week4/team/scripts/teamActivity4.js
+++ week4/team/scripts/teamActivity4.js
@@ -1,22 +1,76 @@
-import {
-    Game
-} from './game.js';
+const board = document.querySelector("#board")
+let cells = board.children
+let turn = 'X'
+let winner = false
+let numTurns = 0
 
-const game = new Game();
+for (i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("touchend", buttonClicked)
+    cells[i].addEventListener("click", buttonClicked)
+}
+
+function buttonClicked() {
+    if (document.getElementById(this.id).textContent.length < 1 && winner == false) {
+
+        document.getElementById(this.id).textContent = turn;
+        numTurns += 1
+        checkForWin(this.id)
+
+        if (numTurns >= 9 && winner == false) {
+            document.querySelector("h1").textContent = "You all lost....."
+        } else {
+            toggleTurn()
+        }
 
 
-const div1 = document.querySelector('#div1');
-div1.innerHTML = 'yessir';
+    }
+}
 
+function toggleTurn() {
+    if (turn == 'X') {
+        turn = 'O'
+    } else {
+        turn = 'X'
+    }
+}
 
-const tbody = document.querySelector('tbody');
-tbody.innerHTML = game.drawBoard();
+document.getElementById("reset").addEventListener("touchend", reset)
+document.getElementById("reset").addEventListener("click", reset)
 
+function reset() {
+    for (i = 0; i < cells.length; i++) {
+        cells[i].textContent = ""
+    }
+    turn = 'X'
+    winner = false
 
+    document.querySelector("h1").textContent = `Tic-Tac-Toe`
+    numTurns = 0
+}
 
-cell0 {
-    htmlRef : "yes",
-    model : 12
-};
+function checkForWin(id) {
+    let classList = document.getElementById(id).classList
 
-var car = {type:"Fiat", model:"500", color:"white"};
+    for (i = 0; i < classList.length; i++) {
+
+        if (classList[i] == 'item') {
+            continue;
+        }
+
+        let items = document.getElementsByClassName(classList[i])
+
+        for (x = 0; x < items.length; x++) {
+            if (items[x].textContent != turn) {
+                break;
+            }
+            if (x == 2) {
+                console.log(turn + " has won")
+
+                document.querySelector("h1").textContent = `${turn} has won`
+
+                winner = true
+
+            }
+        }
+    }
+}
